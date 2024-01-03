7070be6a9b3d56f243b0c8bc1d3289c7c308c59d
Farrux Ro'zmetov <farrukhruzmetov2002@gmail.com>
2024-01-03 20:17:20 +0500

    authentication started

---
+++ Models/AppDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace proeduedge.Models
+{
+    public class AppDBContext : DbContext
+    {
+        
+
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
+        {
+        }
+        public DbSet<Users> Users { get; set; }
+          }
+}
--- Program.cs
+++ Program.cs
@@ -1,15 +1,27 @@
+using System;
+using System.Reflection;
+using proeduedge.Models;
+using Microsoft.EntityFrameworkCore;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+string connString = builder.Configuration.GetConnectionString("proeduedge");
 builder.Services.AddControllers();
-
+//builder.Services.AddScoped<IRepository<Book>, BookRepository>();
+builder.Services.AddDbContext<AppDBContext>(options =>
+{
+    options.UseSqlServer(connString);
+});
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "ProEduEdge", Version = "v1" });
+});
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-
+app.UseSwagger(x => x.SerializeAsV2 = true);
+app.UseSwaggerUI();
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
