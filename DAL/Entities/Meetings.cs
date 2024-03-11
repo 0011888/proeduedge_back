500d2ae6197b22e8ff71d355a21cfac2c2e0ff33
Farrux Ro'zmetov <farrukhruzmetov2002@gmail.com>
2024-03-11 14:24:15 +0500

    tables added

---
+++ DAL/Entities/Meetings.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace proeduedge.DAL.Entities
+{
+    public class Meetings
+    {
+        [Key]
+        public int Id { get; set; }
+        public string RoomId { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
